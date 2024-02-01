fa0b1e6034d0554e4228fd829a4e6bf83221fc10
mohammedasif9786 <asif.jdp@gmail.com>
2024-02-01 14:40:52 +0530

    merging of two list

---
+++ mergeTwoList.js
@@ -0,0 +1,27 @@
+//[1,2,3,4] [1,2,3] = [1,1,2,2,3,3,4]
+
+let mergeList = (inputArr1 ,inputArr2)=>{
+
+    if(mergeList.length == 2){
+
+    let highLen=" "
+        highLen = inputArr1.length > inputArr2.length ? inputArr1.length : inputArr2.length
+
+    let finalArr =[]
+ for(let i=0 ; i<highLen ; i++){
+       if(inputArr1 != undefined){
+         finalArr.push(inputArr1[i])
+         
+         if(inputArr2[i] != undefined)
+         finalArr.push(inputArr2[i])
+       }
+    }
+return finalArr
+}
+else{
+    return []
+}
+
+}
+
+mergeList([1,2,3,4],[1,2,3,4])
